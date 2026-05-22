2c7d770ddcca16b33c2d36129401c3aaa89e2c95
Ritesh <riteshsaud@gmail.com>
2026-05-22 23:00:08 +0530

    add new files

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -6,7 +6,7 @@ public class PrimeNumber{
 		if(n <= 1){
 			isPrime = false;
 		} else {
-			for(int i=2; i<=n/2; i++){
+			for(int i=2; i<=Math.sqrt(n); i++){
 				if(n%i == 0){
 				isPrime = false;
 				break;
